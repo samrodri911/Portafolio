4f0804422913b525c83930fa3b09dbddb5776728
samrodri911 <samrosolar2007@gmail.com>
2026-01-17 15:05:21 -0500

    build para github pages

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/Portafolio/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
